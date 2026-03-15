75e65651f813ffd09f4b22eeca6f019676330fc3
Ayush Kumar <rajayush71576@gmail.com>
2026-03-15 17:15:56 +0530

    Added cloudinary and multer package for image upload on cloud

--- routes/listing.js
+++ routes/listing.js
@@ -2,12 +2,14 @@ const express = require('express');
 const router = express.Router();
 const wrapAsync = require('../utils/wrapAsync.js');
 const { isLoggedIn, isOwner, validateListing } = require('../middleware.js');
-
 const listingController = require('../controllers/listing.js');
+const multer = require('multer');
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage });
 
 router.route('/')
 .get(wrapAsync(listingController.index))
-.post(isLoggedIn, validateListing, wrapAsync(listingController.createListing));
+.post(isLoggedIn, validateListing, upload.single('listing[image]'), wrapAsync(listingController.createListing));
 
 //create route
 router.get('/new', isLoggedIn, listingController.renderNewForm);
