1391e859309080b684ed1f4194587bdae2cb3d73
Ayush Kumar <rajayush71576@gmail.com>
2026-03-26 23:09:34 +0530

    added live location of listing on map

--- public/js/map.js
+++ public/js/map.js
@@ -3,8 +3,8 @@ const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/standard', // Use the standard style for the map
     projection: 'globe', // display the map as a globe
-    zoom: 1, // initial zoom level, 0 is the world view, higher values zoom in
-    center: [77.2088, 28.6139] // center the map on this longitude and latitude
+    zoom: 9, // initial zoom level, 0 is the world view, higher values zoom in
+    center: listing.geometry.coordinates // center the map on this longitude and latitude
 });
 
 map.addControl(new mapboxgl.NavigationControl());
@@ -13,3 +13,9 @@ map.scrollZoom.disable();
 map.on('style.load', () => {
     map.setFog({}); // Set the default atmosphere style
 });
+
+const marker1 = new mapboxgl.Marker({ color: "red" })
+    .setLngLat(listing.geometry.coordinates) //listing.geometry.coordinates
+    .setPopup(new mapboxgl.Popup({offset: 25})
+    .setHTML(`<h4>${listing.title}</h4><p>Exact Location will be provided after booking</p>`))
+    .addTo(map);
